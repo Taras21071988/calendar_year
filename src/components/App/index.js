3d2ab2cd0740bd7d146fed921025acf7c33cd52b
Taras21071988 <tsasojlov@icloud.com>
2023-08-10 16:19:48 +0300

    writing styles to create the desired appearance

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,17 +3,28 @@ import moment from "moment";
 import { Header } from "../Header";
 import { Monitor } from "../Monitor";
 import { CalendarGrid } from "../CalendarGrid";
+import { styled } from "styled-components";
+
+const ShadowWrapper = styled.div`
+  border-radius: 8px;
+  overflow: hidden;
+  border-top: 1px solid #737374;
+  border-left: 1px solid #464648;
+  border-right: 1px solid #464648;
+  border-bottom: 2px solid #464648;
+  box-shadow: 0 0 0 1px #1a1a1a, 0 8px 20px 6px #888
+`;
 
 function App() {
   moment.updateLocale("en", { week: { dow: 1 } });
   const startDay = moment().startOf("month").startOf("week");
 
   return (
-    <div className="App">
+    <ShadowWrapper>
       <Header />
       <Monitor />
       <CalendarGrid startDay={startDay} />
-    </div>
+    </ShadowWrapper>
   );
 }
 
