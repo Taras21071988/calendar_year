f8022a853247cbe783944dd2c154eda0b2211061
Taras21071988 <tsasojlov@icloud.com>
2023-08-12 12:55:31 +0300

    add week names header

--- src/components/CalendarGrid/index.js
+++ src/components/CalendarGrid/index.js
@@ -5,13 +5,13 @@ import { styled } from "styled-components";
 const GridWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(7, 1fr);
-  grid-template-rows: repeat(6, 1fr);
-  background-color: #404040;
+  background-color: ${(props) => (props.$isheader ? "#1e1f21" : "#404040")};
+  ${(props) => props.$isheader && "border-bottom: 1px solid #404040"};
   grid-gap: 1px;
 `;
 const CellWrapper = styled.div`
   min-width: 140px;
-  min-height: 80px;
+  min-height: ${(props) => (props.$isheader ? 24 : 80)}px;
   background-color: ${(props) => (props.$isweekend ? "#272829" : "#1e1f21")};
   color: #dddcdd;
 `;
@@ -19,6 +19,7 @@ const RowCell = styled.div`
   display: flex;
   justify-content: ${(props) =>
     props.$justifycontent ? props.$justifycontent : "flex-start"};
+  ${(props) => props.pr && `padding-right: ${props.pr * 8}px`}
 `;
 const DayWrapper = styled.div`
   height: 33px;
@@ -39,29 +40,41 @@ const CurrentDay = styled.div`
 `;
 
 const CalendarGrid = ({ startDay }) => {
-  //   const totalDays = 42;
   const day = startDay.clone().subtract(1, "day");
   const daysArray = [...Array(42)].map(() => day.add(1, "day").clone());
-  const isCurrentDay = (day) => moment().isSame(day,'day');
+  const isCurrentDay = (day) => moment().isSame(day, "day");
 
   return (
-    <GridWrapper>
-      {daysArray.map((dayItem) => (
-        <CellWrapper
-          key={dayItem.format("DDMMYYYY")}
-          $isweekend={dayItem.day() === 6 || dayItem.day() === 0}
-        >
-          <RowCell $justifycontent={"flex-end"}>
-            <DayWrapper>
-              {!isCurrentDay(dayItem) && dayItem.format("D")}
-              {isCurrentDay(dayItem) && (
-                <CurrentDay>{dayItem.format("D")}</CurrentDay>
-              )}
-            </DayWrapper>
-          </RowCell>
-        </CellWrapper>
-      ))}
-    </GridWrapper>
+    <>
+      <GridWrapper $isheader>
+        {[...Array(7)].map((_, i) => (
+          <CellWrapper $isheader>
+            <RowCell $justifycontent={"flex-end"} pr={1}>
+              {moment()
+                .day(i + 1)
+                .format("ddd")}
+            </RowCell>
+          </CellWrapper>
+        ))}
+      </GridWrapper>
+      <GridWrapper>
+        {daysArray.map((dayItem) => (
+          <CellWrapper
+            key={dayItem.format("DDMMYYYY")}
+            $isweekend={dayItem.day() === 6 || dayItem.day() === 0}
+          >
+            <RowCell $justifycontent={"flex-end"}>
+              <DayWrapper>
+                {!isCurrentDay(dayItem) && dayItem.format("D")}
+                {isCurrentDay(dayItem) && (
+                  <CurrentDay>{dayItem.format("D")}</CurrentDay>
+                )}
+              </DayWrapper>
+            </RowCell>
+          </CellWrapper>
+        ))}
+      </GridWrapper>
+    </>
   );
 };
 
